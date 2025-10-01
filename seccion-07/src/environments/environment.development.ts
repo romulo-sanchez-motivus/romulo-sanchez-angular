4311f28061b8d2791d8ca07e49dd080ca30ed995
Romulo Sanchez <sanchez.systems.24@gmail.com>
2025-10-01 17:56:41 -0600

    seccion 7

---
+++ seccion-07/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  // ApiKeys
+  // https://developers.giphy.com/dashboard/
+  giphyApiKey: 'vgN2OeVYnojQWA0mFrXSkPyUuUE68kc5',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
