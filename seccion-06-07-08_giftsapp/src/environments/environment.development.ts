b030d1b42967cff8281433d172f69f4b339197d3
Romulo Sanchez <sanchez.systems.24@gmail.com>
2025-10-01 18:13:14 -0600

    seccion 06-07-09 giftsapp

---
+++ seccion-06-07-08_giftsapp/src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+  giphyApiKey: 'vgN2OeVYnojQWA0mFrXSkPyUuUE68kc5',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
--- seccion-06/src/app/gifs/pages/search-page/search-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-search-page',
-  imports: [],
-  templateUrl: './search-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export default class SearchPageComponent {}
