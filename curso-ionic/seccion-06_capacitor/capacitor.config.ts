2eb54688000ad80e818339049a8b5f85ab879d52
Romulo Sanchez <sanchez.systems.24@gmail.com>
2025-10-09 11:08:32 -0600

    ionic seccion 06
    - seccion 6:
        capacitor

---
+++ curso-ionic/seccion-06_capacitor/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor_example',
+  webDir: 'www'
+};
+
+export default config;
